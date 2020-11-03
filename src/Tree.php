930370d858043ec2c0a831575d88128637fec31f
cistos <infernal_itachi@mail.ru>
2020-11-03 06:26:37 +0200

    Added composer.json, updated .gitignore, created Tree constructor

--- src/Tree.php
+++ src/Tree.php
@@ -2,10 +2,19 @@
 
 namespace Seekwoyee;
 
+use Seekwoyee\Trees\AbstractTreeManager;
+
 /**
  * Class Tree - класс-компоновщик для любых деревьев
  * @package Seekwoyee
  */
 class Tree {
+    private array $nodes = [];
+    public AbstractTreeManager $treeManager;
+    private int $rootId;
+    private string $type;
 
+    public function __construct(string $type, array $nodeParams)
+    {
+    }
 }
